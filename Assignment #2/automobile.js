e4e1570a036bc89991ae15e6dfccbabebdde762d
wellungt <wellungt@oregonstate.edu>
2019-07-14 16:42:23 -0700

    Update automobile.js

--- Assignment #2/automobile.js
+++ Assignment #2/automobile.js
@@ -1,8 +1,16 @@
-function Automobile( year, make, model, type ){
+function Automobile(year, make, model, type ){
     this.year = year; //integer (ex. 2001, 1995)
     this.make = make; //string (ex. Honda, Ford)
     this.model = model; //string (ex. Accord, Focus)
     this.type = type; //string (ex. Pickup, SUV)
+
+    this.logMe = function logMe(arg){
+      if (arg == true) {
+            console.log(this.year + ' ' + this.make + ' ' + this.model + ' ' + this.type);
+        } else {
+            console.log(this.year + ' ' + this.make + ' ' + this.model);
+        }
+      }
 }
 
 var automobiles = [
@@ -14,18 +22,42 @@ var automobiles = [
     new Automobile(2008, "Subaru", "Outback", "Wagon")
     ];
 
+
+console.log("------------------------------");
+sortArr(yearComparator, automobiles);
+console.log("The cars sorted by year are:");
+automobiles.forEach((obj) => obj.logMe(false));
+console.log("------------------------------");
+sortArr(makeComparator, automobiles);
+console.log("The cars sorted by make are:");
+automobiles.forEach((obj) => obj.logMe(false));
+console.log("------------------------------");
+sortArr(typeComparator, automobiles);
+console.log("The cars sorted by type are:");
+automobiles.forEach((obj) => obj.logMe(true));
+console.log("------------------------------");
+
 /* This function sorts arrays using an arbitrary comparator. You pass it a
  * comparator and an array of objects appropriate for that comparator and it
  * will return a new array which is sorted with the largest object in index 0
  * and the smallest in the last index */
-function sortArr( comparator, array ){
+function sortArr(comparator, array){
     /*your code here*/
+    for (var i = 0; i < array.length; ++i) {
+        for (var j = 0; j < array.length - 1; ++j) {
+            if(!comparator(array[j], array[j + 1])) {
+                var tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
 }
 
 /* A comparator takes two arguments and uses some algorithm to compare them.
  * If the first argument is larger or greater than the 2nd it returns true,
  * otherwise it returns false. Here is an example that works on integers */
-function exComparator( int1, int2){
+function exComparator(int1, int2){
     if (int1 > int2){
         return true;
     } else {
@@ -39,15 +71,27 @@ function exComparator( int1, int2){
 
 /* This compares two automobiles based on their year. Newer cars are "greater"
  * than older cars. */
-function yearComparator( auto1, auto2){
+function yearComparator(auto1, auto2){
     /* your code here*/
+    if (auto1.year > auto2.year){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /* This compares two automobiles based on their make. It should be case
  * insensitive and makes which are alphabetically earlier in the alphabet are
  * "greater" than ones that come later. */
-function makeComparator( auto1, auto2){
+function makeComparator(auto1, auto2){
     /* your code here*/
+    var make1 = (auto1.make).toLowerCase();
+    var make2 = (auto2.make).toLowerCase();
+    if (make1 < make2){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /* This compares two automobiles based on their type. The ordering from
@@ -55,8 +99,18 @@ function makeComparator( auto1, auto2){
  * (types not otherwise listed). It should be case insensitive. If two cars are
  * of equal type then the newest one by model year should be considered
   * "greater". */
-function typeComparator( auto1, auto2){
+function typeComparator(auto1, auto2){
     /* your code here*/
+    var types = ["wagon", "suv", "pickup", "roadster"];
+    var type1 = types.indexOf((auto1.type).toLowerCase());
+    var type2 = types.indexOf((auto2.type).toLowerCase());
+    if (type1 > type2){
+        return true;
+    } else if (type1==type2){
+        return yearComparator(auto1,auto2);
+    } else {
+        return false;
+    }
 }
 
 /* Your program should output the following to the console.log, including the
